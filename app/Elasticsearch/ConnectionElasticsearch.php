ad4a8f73648a70f3627ebdff2af02bc8adadd5bb
sina1010anis <sina1010anis@gmail.com>
2023-09-27 18:18:10 +0330

    edit file

--- app/Elasticsearch/ConnectionElasticsearch.php
+++ app/Elasticsearch/ConnectionElasticsearch.php
@@ -2,14 +2,19 @@
 namespace App\Elasticsearch;
 
 use Elastic\Elasticsearch\ClientBuilder;
+use Elastic\Elasticsearch\Exception\ClientResponseException;
+use Illuminate\Support\Facades\Http;
 
 class ConnectionElasticsearch
 {
     protected $connetion_data;
+    protected $where_data;
+
 
     public function __construct()
     {
 
+        // Connect to elasticsearch using two variables ELASTICSEARCH_USERNAME and ELASTICSEARCH_PASSWORD
         $this->connetion_data = ClientBuilder::create()
             ->setBasicAuthentication(env('ELASTICSEARCH_USERNAME'), env('ELASTICSEARCH_PASSWORD'));
 
@@ -17,7 +22,7 @@ class ConnectionElasticsearch
 
     public function connction()
     {
-
+        // Check the connection
         return $this->connetion_data;
 
     }
@@ -32,30 +37,51 @@ class ConnectionElasticsearch
     public function connctionVersion()
     {
 
+        // Check the version
         return $this->connctionBuild()->info()['version']['number'];
 
     }
 
-    public function connctionIndex(array $data)
+    public function forDatas(array $datas, string $keyArray): array
     {
-
+        $push = [];
+        foreach ($datas as $data) {
+            $push[$keyArray] = $data;
+        }
+        return $push;
+    }
+    public function connctionIndex(string $index, array $datas = [], string|null $type = null)
+    {
+        $type = ($type == null) ? $index : $type;
+        //Save to elasticsearch
         // $data exampel =>
-            // $params = [
-            //     'body' => [
-            //         'title' => 'this is Elasticsearch',
-            //         'body' => 'this is Elasticsearch Body In page',
-            //         'view' => 56,
-            //     ],
-            //     'index' => 'my_test',
-            //     'type' => 'my_test',
-            // ];
-        return $this->connctionBuild()->index($data);
+        $params = [
+            'body' => $datas,
+            'index' => $index,
+            'type' => $type,
+        ];
+        return $this->connctionBuild()->index($params);
 
     }
 
-    public function connctionSearch(array $data, bool $show_only_data = false)
+    public function connectionWhere(array $wheres = [], string $index = '_all')
     {
+        // $wheres = [['brand' => 'BMW'], ['model' => '730i']]
+        $this->where_data = ($wheres == []) ? ['index' => $index] : [
+            'index' => $index,
+            'body' => [
+                'query' => $this->forDatas($wheres, 'match')
+            ]
+        ];
+        return $this;
+    }
+    public function connctionSearch(bool $show_only_data = false, bool $show_shard = false)
+    {
+        // If show_only_data is false, it will display the data in raw form
+        // If show_shard is true, it will display the data from the _source layer
 
+        //Select to elasticsearch
+        $data_only = [];
         // $data exampel =>
             // $params = [
             //     'index' => 'my_test',
@@ -67,13 +93,54 @@ class ConnectionElasticsearch
             //         ]
             //     ]
             // ];
-
-        $res = $this->connctionBuild()->search($data);
-        if ($show_only_data) {
-            return $res['hits']['hits'];
+        try {
+            $res = $this->connctionBuild()->search($this->where_data);
+            if ($show_only_data) {
+                if ($show_shard) {
+                    foreach ($res['hits']['hits'] as $source) {
+                        $data_only[] = $source['_source'];
+                    }
+                }
+                return ($show_shard) ? $data_only : $res['hits']['hits'];
+            }
+        } catch (ClientResponseException $e) {
+            return 'Error: '. $e->getMessage();
         }
         return $res;
 
     }
 
+    public function connectionDelete(string $index, $id)
+    {
+        try {
+            $delete = $this->connctionBuild()->delete([
+                'index' => $index,
+                'id' => $id
+            ]);
+            if ($delete) {
+                return true;
+            }
+            return 'id: ('.$id.') '.'not found...!';
+        } catch (ClientResponseException $e) {
+            return $e->getMessage();
+        }
+
+    }
+
+    public function connectionGet(string $index, $id)
+    {
+        return $this->connctionBuild()->get(['index' => $index, 'id' => $id])['_source'];
+    }
+
+    public function connectionUpdate(string $index, $id, array $new_datas)
+    {
+        try {
+            $params = ['index' => $index,'id' => $id, 'body' => ['doc' => $new_datas]];
+
+            // Update doc at /my_index/_doc/my_id
+            $this->connctionBuild()->update($params);
+        } catch (ClientResponseException $e) {
+            return $e->getMessage();
+        }
+    }
 }
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,86 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Elastic\Elasticsearch\ClientBuilder;
 use App\Elasticsearch\ConnectionElasticsearch as Elastic;
+use Elastic\Elasticsearch\Client;
 
 class IndexController extends Controller
 {
     public function index(Elastic $elastic)
     {
+        //$elastic->connctionIndex('my_test', ['brand' => 'KMC', 'model' => 'J7', 'price' => 9870]);
+        //$elastic->connctionIndex('my_test', ['brand' => 'KMC', 'model' => 'J7', 'price' => 9870]);
 
-
-
-
-        // $params = [
-        //     'body' => [
-        //         'title' => 'this is Elasticsearch',
-        //         'body' => 'this is Elasticsearch Body In page',
-        //         'view' => 56,
-        //     ],
-        //     'index' => 'my_test',
-        //     'type' => 'my_test',
-        // ];
-        // $res = $elastic->connctionIndex($params);
-        // dd($res);
-
-        $params_created = [
-            'index' => 'my_test',
-            'body'  => [
-                'brand' => 'BMW',
-                'model' => '730i',
-                'location' => 'Deutschland',
-                'price' => 10500
-            ],
-        ];
-
-        $params_search = [
-            'index' => 'my_test',
-            // 'body' => [
-            //     'query' => [
-            //         'match' => [
-            //             'brand' => 'BMW'
-            //         ]
-            //     ]
-            // ]
-        ];
-        //$res = $elastic->connctionIndex($params_created);
-        $res = $elastic->connctionSearch($params_search, true);
-        //dd($res[0]);
-        foreach ($res as $source) {
-            foreach ($source['_source'] as $val) {
-                echo $val.'<br>';
-            }
-            echo '<hr>';
-        }
-        //dd($res[0]['_source']['brand']);
-
-        // $client = ClientBuilder::create()
-        //     ->setBasicAuthentication('elastic', 'sina1010anis');
-        //     //->setHosts(['localhost:9200'])
-
-        // // Info API
-        // $response = $client->build()->info();
-
-        // dd($client->build()->info()['version']['number']);
-        // $key = 'c06747f730544a3babf2ec828e7348b2';
-        // //dd(Http::post('http://localhost:9200/', ['username' => 'elastic', 'password' => 'sina1010anis']));
-
-        // $client = ClientBuilder::create()->setBasicAuthentication('elastic', 'sina1010anis')->build();
-        // //$client = ClientBuilder::create()->build();
-        // $parms = [
-        //     'body' => [
-        //         'title' => 'test elasticsearch',
-        //         'price' => 'Free',
-        //         'location' => 'Iran',
-        //     ],
-        //     'index' => 'mysite',
-        //     'type' => 'mysite',
-        // ];
-        // $res = $client->info();
-        // $client->index($parms);
-        // Info API
-        //dd($res);
-        //return 'Hello World...!';
+        // Update doc at /my_index/_doc/my_id
+        //$elastic->connectionUpdate('my_test', 'k8lDzYoBIXxiJTtJNurl', ['brand' => 'KMC', 'model' => 'J7 pro']);
+        //$elastic->connectionDelete('my_test', 'Mb8nyIoB4xkBd1YQzcIU');
+        //$res = $elastic->connectionWhere([['brand' => 'Benz']], 'my_test')->connctionSearch(true);
+        $res = $elastic->connectionGet('my_test', 'Nr8pyIoB4xkBd1YQcsJi');
+        dd($res);
+        // dump($response['hint']['hint']);
 
     }
 }
